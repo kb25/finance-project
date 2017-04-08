97e8e4357ab16142bcda54266f2232bebe660f3c
Raymond B Matovu <raymond.baziwane@gmail.com>
2017-04-08 12:46:54 -0500

    Fixed tests

--- tests/serugees-tests/UnitTest1.cs
+++ tests/serugees-tests/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace serugees_tests
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(4, Add(2, 2));
         }
 
         int Add(int x, int y)
